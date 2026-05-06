ac91252ecd0b450133bd1c5bf8af114a4c58d7ba
Risekenje Herrera <herrerarisekenje@gmail.com>
2026-05-06 21:45:38 +0800

    Add files via upload

---
+++ dart/Boarding_house/Day01 variable/main.dart
@@ -0,0 +1,14 @@
+void main(){
+  print("Welcome to my Boarding House");
+
+String ownerName = "Kenje";
+String cpnumber = "09911027584";
+double monthlyRent = 5000;
+int tenantsAllowed = 2;
+bool wifi = true;
+  print("Owner name: $ownerName");
+  print("Cellphone number: $cpnumber");
+  print("MonthlyRent: $monthlyRent");
+  print("TenantsAllowed: $tenantsAllowed");
+  print("It has a wifi?: $wifi");
+}
